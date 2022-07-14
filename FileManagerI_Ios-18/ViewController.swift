a26143d396cb0c752a0df3c3a1f7f759c1462daf
Nariman05Manas <89251464474@mail.ru>
2022-07-14 20:26:41 +0300

    Initial Commit

---
+++ FileManagerI_Ios-18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManagerI_Ios-18
+//
+//  Created by qwerty on 14.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
